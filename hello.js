6182f94aef410a38cfbff7be5b3052a9e4a4e3ad
rebeenrebwar <rebeenrebwar@gmail.com>
2016-03-31 13:27:38 -0400

    Added files via upload

---
+++ hello.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    'use strict';
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.end('Hello World\n'); // missing semi-colon will fail the build
+}).listen(1337, '127.0.0.1'); // reporting to the console
+
+// feedback to the user
+console.log('Server running at https://127.0.0.1:1337');
+// this is EOF
